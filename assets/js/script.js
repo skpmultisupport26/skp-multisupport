1fa0cd5209b44aa93ef70373403827e22f1e44ec
SKP MULTISUPPORT <skpmultisupport26@gmail.com>
2026-07-16 21:26:08 +0530

    Initial Website Upload

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+// Mobile Menu
+const menu = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".navbar ul");
+
+if (menu && nav) {
+    menu.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+}
+
+// Counter Animation
+window.addEventListener("load", () => {
+
+    const counters = document.querySelectorAll(".stat-box h2");
+
+    counters.forEach(counter => {
+
+        const target = parseInt(counter.textContent);
+
+        let count = 0;
+
+        const updateCounter = () => {
+
+            const increment = Math.ceil(target / 100);
+
+            if (count < target) {
+
+                count += increment;
+
+                if (count > target) count = target;
+
+                counter.textContent = count + "+";
+
+                setTimeout(updateCounter, 20);
+
+            } else {
+
+                counter.textContent = target + "+";
+
+            }
+
+        };
+
+        counter.textContent = "0+";
+        updateCounter();
+
+    });
+
+});// Contact Form Message
+
